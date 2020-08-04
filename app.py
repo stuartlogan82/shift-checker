7ab135b7baa9180f0ee23005417a77c72d6ae929
stuartlogan82 <slogan@twilio.com>
2020-08-04 13:23:41 +0100

    add basic auth function

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 import connexion
-
+import os
 
 app = connexion.App(__name__, specification_dir="./")
 
 app.add_api('swagger.yml')
+
+
+def basic_auth(username, password, required_scopes=None):
+    if username == os.environ.get("API_USERNAME") and password == os.environ.get("API_PASSWORD"):
+        return {'Auth', 'OK'}
+
+    return None
 
 
 @app.route('/')
